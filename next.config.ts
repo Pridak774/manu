4c66b9aea5eb584596737ccf861945579a378052
Emanuel Astalos <emanuelastalos@Emanuels-MacBook-Pro.local>
2025-05-06 14:36:33 +0300

    fix: adjust Netlify publish directory and Next.js output config

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["http://192.168.1.134:3000"],
-  output: 'standalone',
   typescript: {
-    ignoreBuildErrors: true, // Added this section
+    ignoreBuildErrors: true,
   },
 };
 
